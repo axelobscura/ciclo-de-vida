a881958d9e70d1d2c819b887d0bf029f48dd99f2
Axel <axosar@gmail.com>
2022-12-01 11:37:50 -0600

    test

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import { useState, useRef } from "react";
+import Head from "next/head";
+
+import styles from "../styles/styles.module.scss";
+import FormCard from "../components/FormCard";
+import {
+  BillingInfo,
+  ConfirmPurchase,
+  PersonalInfo
+} from "../components/Forms";
+import FormCompleted from "../components/FormCompleted";
+
+const App = () => {
+  const [formStep, setFormStep] = useState(0);
+
+  const nextFormStep = () => setFormStep((currentStep) => currentStep + 1);
+
+  const prevFormStep = () => setFormStep((currentStep) => currentStep - 1);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>DISEÑANDO MI CASA</title>
+      </Head>
+      <FormCard currentStep={formStep} prevFormStep={prevFormStep}>
+        {formStep >= 0 && (
+          <PersonalInfo formStep={formStep} nextFormStep={nextFormStep} />
+        )}
+        {formStep >= 1 && (
+          <BillingInfo formStep={formStep} nextFormStep={nextFormStep} />
+        )}
+        {formStep >= 2 && (
+          <ConfirmPurchase formStep={formStep} nextFormStep={nextFormStep} />
+        )}
+
+        {formStep > 2 && <FormCompleted />}
+      </FormCard>
+    </div>
+  );
+};
+
+export default App;
